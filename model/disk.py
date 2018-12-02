ac0c24c334333cfcb4de2cf4d5738f029162156f
ssynn <824063458@qq.com>
2018-12-02 22:40:27 +0800

    磁盘接口初步定义完成

--- model/disk.py
+++ model/disk.py
@@ -1,12 +1,10 @@
 
 # 所有和磁盘操作有关的方法都在此模块中
 # 1.查询文件状态
-#   1.1 创建时间
-#   1.2 大小
-#   1.3 名称
-#   1.4 修改时间
-#   1.5 文件路径
-#   1.6 只读
+#   1.1 大小
+#   1.2 名称
+#   1.3 属性
+#   1.4 建立时间
 # 2.创建文件夹
 # 3.创建文件
 # 4.删除文件
@@ -18,118 +16,132 @@
 #   9.1 磁盘大小
 #   9.2 磁盘占用
 #   9.3 磁盘扇区占用情况
+# 10.移动文件/文件夹
 
 
 # 磁盘属性
-# 400个扇区
+# 128个扇区
 # 每个扇区64B
 # 每个文件最大64B
-# 系统占用40个扇区
-# C盘从41开始到220结束
-# D盘从221开始到400结束
-
-
-# mes = {
-#     'create_time': str,
-#     'name': str,
-#     'ext': str,
-#     'path': str,
-#     'size': int,            # 大小为64B的整数倍
-#     'edit_time': str,
-#     'read_only': int
-# }
-
-import time
-import json
+# 系统占用3个扇区
+# C盘
+# D盘
 
 
 # 创建文件
-def mkfile(mes: dict):
-    # file_tree = getTree()
-    # path = mes['path'].split('/')
-    pass
-
-
-# 创建文件夹
-def mkdir(path: str):
+def create_file(mes: dict) -> bool:
+    '''
+    传入字典{
+        name: str,
+        ext: str,
+        text: str,
+        attribute: int
+    }
+    '''
     pass
 
 
 # 删除文件
-def rmfile(path: str):
-    pass
-
-
-# 删除文件夹
-def rmdir(path: str):
-    pass
-
-
-# 获取文件列表
-def listdir(path: str):
+def remove_file(path: str) -> bool:
+    '''
+    传入文件路径
+    '''
     pass
 
 
 # 修改文件
-def modifyfile(path: str, new_mes: dict):
+def modify_file(path: str, new_mes: dict) -> bool:
+    '''
+    传入文件路径和文件信息{
+        name: str,
+        ext: str,
+        text: str,
+        attribute: int
+    }
+    '''
+    pass
+
+
+# 打开文件
+def open_file(path: str) -> dict:
+    '''
+    传入文件路径
+    返回文件信息{
+        name: str,
+        ext: str,
+        text: str,
+        time: str,
+        attribute: int
+    }
+    '''
+    pass
+
+
+# 获取文件列表
+def list_dir(path: str) -> list:
+    '''
+    传入文件夹路径
+    返回当前文件夹内的所有文件和文件夹[str, str, ...]
+    '''
     pass
 
 
 # 修改文件夹
-def modifydir(path: str, new_name: str):
+def modify_dir(path: str, new_name: str) -> bool:
+    '''
+    传入原始路径和文件夹的新名字
+    '''
     pass
+
+
+# 创建文件夹
+def creat_dir(path: str) -> bool:
+    '''
+    传入文件夹路径
+    '''
+    pass
+
+
+# 移动
+def move(now_path: str, new_path: str) -> bool:
+    '''
+    传入旧地址和新地址
+    '''
+
+
+# 获取磁盘内容
+def get_disk(disk: str = 'c'):
+    '''
+    传入磁盘名，打印出磁盘内所有信息
+    '''
+    with open('virtual_disk_'+disk, 'rb') as d:
+        f = d.readline()
+    print(f, len(f))
 
 
 # 获取磁盘信息
-def getDiskMes():
+def disk_info(disk: str = 'c') -> list:
+    '''
+    传入磁盘名，默认为c盘
+    以列表的形式返回磁盘表项
+    '''
     pass
-
-
-# 获取文件信息
-def getFileMes(path: str):
-    pass
-
-
-# 获取文件树
-def getTree():
-    ans = ""
-    with open('virtual_disk', 'r+') as d:
-        for i in range(40):
-            ans += (d.readline()[:-1])
-        p = len(ans) - 1
-        while ans[p] != '}':
-            p -= 1
-        ans = ans[:p+1]
-
-    root = json.loads(ans)
-    return root
 
 
 # 格式化磁盘
-def formatDisk():
-    with open('virtual_disk', 'w') as disk:
-        for i in range(400):
+def format_disk(disk: str = 'c') -> bool:
+    '''
+    传入磁盘号，默认为c盘
+    '''
+    with open('virtual_disk_' + disk, 'w') as d:
+        for i in range(128):
             for j in range(64):
-                disk.write('0')
-            disk.write('\n')
-    tree = {
-        'root': ['C:', 'D:'],
-        'END': 20,
-        'size': 25600,          # 单位字节
-        'allocated': 2560,
-        'c_allocated': 0,
-        'd_allocated': 0,
-        'C:': [],
-        'D:': [],
-    }
+                d.write('0')
+            d.write('\n')
 
-    tree_json = json.dumps(tree)
-    mes_list = divide(tree_json)
-
-    with open('virtual_disk', 'r+') as d:
-        for i in range(len(mes_list)):
-            d.seek(66*i)
-            d.writelines(mes_list[i])
+    with open('virtual_disk_' + disk, 'rb+') as d:
+        d.seek(0)
+        d.write(bytes([129, 129, 129]))
 
 
 # 字符串分割
@@ -144,14 +156,4 @@ def divide(val):
 
 
 if __name__ == '__main__':
-    # formatDisk()
-    # print(getTree())
-    mes = {
-        'create_time': time.strftime('%Y-%m-%d %H:%M:%S'),
-        'name': 'a.txt',
-        'ext': '.txt',
-        'path': 'C:/a.txt',
-        'size': '64',            # 大小为64B的整数倍
-        'edit_time': time.strftime('%Y-%m-%d %H:%M:%S'),
-        'read_only': 0
-    }
+    format_disk()
---
+++ model/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+en('virtual_disk_c', 'rb') as d:
+        f = d.readline()
+
+    print(f)
